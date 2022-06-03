88547d9b84fd608395724630e8af860cae3e4151
Vanessa Vieira <vanesvieira@gmail.com>
2022-06-03 11:24:38 -0300

    atualizações

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const btnMenu = document.getElementById('btn-menu');
+const closeMenu = document.getElementById('btn-close');
 
 function toggleMenu() {
     const nav = document.getElementById('navigation');
     nav.classList.toggle('active');
 }
 
+function close() {
+    const nav = document.getElementById('navigation');
+    nav.classList.close('remove');
+}
+
 btnMenu.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', close);
